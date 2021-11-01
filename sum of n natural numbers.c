b1b0930adc3a5b505a056b54fa1a530afa2e9455
GaneshKranthVarma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-01 10:58:23 +0530

    Add files via upload

---
+++ sum of n natural numbers.c
@@ -0,0 +1,9 @@
+/* sum of n natural numbers*/
+#include<stdio.h>
+void main()
+{
+	int n;
+	n=(n+1)/2;
+	printf("sum of first 5 natural numbers is:%d",n);
+	printf("sum of first 10 natural numbers is %d",n);
+}
