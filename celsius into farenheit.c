b92b9ee693f3d0c383ab8a4d298dd9fa93995c37
GaneshKranthVarma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-03 10:58:28 +0530

    Update celsius into farenheit.c

--- celsius into farenheit.c
+++ celsius into farenheit.c
@@ -1,9 +1,9 @@
 /*celsius to farenheit*/
 #include<stdio.h>
-void main()
+float main()
 { int c,f;
 c=10;
 f=(c*9/5)+32;
-printf("if celsius is %d then farenheit is %d",c,f);
+printf("if celsius is %.2f then farenheit is %.2f",c,f);
 
 }
