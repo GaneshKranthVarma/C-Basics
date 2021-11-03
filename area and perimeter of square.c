cb05f8e6bdddf548b1e35b46b642d202aa0473c6
GaneshKranthVarma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-03 19:05:02 +0530

    Update area and perimeter of square.c

--- area and perimeter of square.c
+++ area and perimeter of square.c
@@ -1,11 +1,13 @@
+/*Area and perimeter of square*/
 #include<stdio.h>
 void main()
 {
-Int s,a,p;
+int s,a,p;
 s=2;
 a=s*s;
 p=4*s;
-Printf("area of square is %d if side is %d",a,s);
-Printf("perimeter of square is %d if side is %d",p,s);
+printf("area of square is %d if side is %d\n",a,s);
+printf("perimeter of square is %d if side is %d",p,s);
 	
 }
+
