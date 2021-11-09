3a7d1bb1e468d6c04ca926473300ece65dda8f12
Ganesh Kranth Varma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-09 10:29:07 +0530

    Add files via upload

---
+++ scanf Multi input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+   int a,b,c;
+   printf("Enter three numbers");
+   scanf("%d%d%d", &a, &b, &c);
+   printf("Number 1: %d\n",a);
+   printf("Number 2: %d\n",b);
+   printf("Number 3: %d\n",c);
+  
+}
