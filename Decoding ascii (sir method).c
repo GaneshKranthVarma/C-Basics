304e7af92d42abefdd5c3579192e64ccd3141c49
Ganesh Kranth Varma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-15 10:45:22 +0530

    Add files via upload

---
+++ Decoding ascii (sir method).c
@@ -0,0 +1,9 @@
+/* Decoding Ascii (sir method) */
+#include<stdio.h>
+void main()
+{ 
+    int a,b,c;
+    printf("Enter three numbers");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("%c%c%c", a+95, b+95, c+95);
+}
