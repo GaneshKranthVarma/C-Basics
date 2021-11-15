37a145787e84668e0f5c484dff1534c5744c4e16
Ganesh Kranth Varma <93191154+GaneshKranthVarma@users.noreply.github.com>
2021-11-15 10:34:12 +0530

    Add files via upload

---
+++ Decoding ascii numbers.c
@@ -0,0 +1,16 @@
+/* Decoding Ascii numbers */
+#include<stdio.h>
+int main()
+{ 
+ //input for cat//
+    int x=95,a,b,c,d,e,f;
+    a=4;
+    b=2;
+    c=21;
+    printf("%c%c%c\n",x+a,x+b,x+c);
+ //input for dog//
+    d=5;
+    e=16;
+    f=8;
+    printf("%c%c%c",x+d,x+e,x+f);
+}
